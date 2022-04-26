8abfcb3e61a6b6895ecbcdf160a3e9fbd72ca7a7
jiyang <jiyangmax@163.com>
2022-04-26 22:20:08 +0800

    init netty

--- src/main/java/com/github/chatroom/ChatRoomApplication.java
+++ src/main/java/com/github/chatroom/ChatRoomApplication.java
@@ -1,4 +1,4 @@
-package com.example.githubactionsjavatest;
+package com.github.chatroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
